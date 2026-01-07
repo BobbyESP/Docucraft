eb34c6e40af9f83d1187d14f2de3d79e1e50faae
Gabriel Fontán Rodiño <gabilessto@gmail.com>
2026-01-07 22:52:04 +0100

    refactor: improve versioning structure and code readability in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,72 +13,82 @@ plugins {
     //alias(libs.plugins.androidTest) apply false
 }
 
-val commitSignature = providers.exec {
-    commandLine("git", "rev-parse", "--short", "HEAD")
-}.standardOutput.asText.get().substringBefore("\n")
-
 sealed class Version(
-    open val versionMajor: Int,
-    val versionMinor: Int,
-    val versionPatch: Int,
-    val versionBuild: Int = 0,
-    val commitId: String = ""
+    open val major: Int,
+    open val minor: Int,
+    open val patch: Int,
+    open val build: Int = 0
 ) {
-    abstract fun toVersionName(): String
+    protected abstract val stageCode: Int // 0: Alpha, 1: Beta, 2: RC, 3: Stable
+    protected abstract val stageName: String
 
-    fun toVersionCode(
-        extraDigit: String = "",
-    ): Int {
-        val minorExtraDigit = if (versionMinor > 9) {
-            (versionMinor / 10).toString()
+    fun toVersionName(): String {
+        return if (this is Stable) {
+            "$major.$minor.$patch"
         } else {
-            ""
+            "$major.$minor.$patch-$stageName.$build"
         }
-
-        return "$versionMajor$minorExtraDigit$versionPatch$versionBuild$extraDigit".toInt()
     }
 
-    class Stable(versionMajor: Int, versionMinor: Int, versionPatch: Int) :
-        Version(versionMajor, versionMinor, versionPatch) {
-        override fun toVersionName(): String = "${versionMajor}.${versionMinor}.${versionPatch}"
+    /**
+     * Format: MMMMmmPPsBB (Major - Minor - Patch - stage - Build)
+     * Major: Max. 200 - Example
+     * Minor: Max. 99
+     * Patch: Max. 99
+     * Stage: 0-3
+     * Build: 0-99
+     */
+    fun toVersionCode(): Int {
+        return (major * 10_000_000) + (minor * 100_000) + (patch * 1_000) + (stageCode * 100) + build
     }
 
-    class ReleaseCandidate(
-        versionMajor: Int, versionMinor: Int, versionPatch: Int, versionBuild: Int
-    ) : Version(versionMajor, versionMinor, versionPatch, versionBuild) {
-        override fun toVersionName(): String =
-            "${versionMajor}.${versionMinor}.${versionPatch}-rc.$versionBuild"
+    data class Alpha(
+        override val major: Int,
+        override val minor: Int,
+        override val patch: Int,
+        override val build: Int
+    ) : Version(major, minor, patch, build) {
+        override val stageCode = 0
+        override val stageName = "alpha"
     }
 
-    class Beta(versionMajor: Int, versionMinor: Int, versionPatch: Int, versionBuild: Int) :
-        Version(versionMajor, versionMinor, versionPatch, versionBuild) {
-        override fun toVersionName(): String =
-            "${versionMajor}.${versionMinor}.${versionPatch}-beta.$versionBuild"
+    data class Beta(
+        override val major: Int,
+        override val minor: Int,
+        override val patch: Int,
+        override val build: Int
+    ) : Version(major, minor, patch, build) {
+        override val stageCode = 1
+        override val stageName = "beta"
     }
 
-    class Alpha(
-        versionMajor: Int, versionMinor: Int, versionPatch: Int, commitId: String,
-    ) : Version(versionMajor, versionMinor, versionPatch, commitId = commitId) {
-        override fun toVersionName(): String =
-            "${versionMajor}.${versionMinor}.${versionPatch}-alpha.$commitId"
+    data class ReleaseCandidate(
+        override val major: Int,
+        override val minor: Int,
+        override val patch: Int,
+        override val build: Int
+    ) : Version(major, minor, patch, build) {
+        override val stageCode = 2
+        override val stageName = "rc"
+    }
+
+    data class Stable(
+        override val major: Int,
+        override val minor: Int,
+        override val patch: Int
+    ) : Version(major, minor, patch) {
+        override val stageCode = 3
+        override val stageName = ""
+        override val build = 0
     }
 }
 
 val currentVersion: Version = Version.Beta(
-    versionMajor = 1,
-    versionMinor = 0,
-    versionPatch = 0,
-    versionBuild = 3,
+    major = 1,
+    minor = 0,
+    patch = 0,
+    build = 4
 )
 
-val versionCode by extra(
-    currentVersion.toVersionCode(
-        extraDigit = when(currentVersion) {
-            is Version.Stable -> ""
-            is Version.ReleaseCandidate -> "${currentVersion.versionBuild}"
-            is Version.Beta -> "${currentVersion.versionBuild}"
-            is Version.Alpha -> currentVersion.commitId.filter { it.isDigit() }
-        }
-    )
-)
+val versionCode by extra(currentVersion.toVersionCode())
 val versionName by extra(currentVersion.toVersionName())
