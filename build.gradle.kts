bdce5c43d4d422068eb14ac3a2e8ace370f7e4fb
Gabriel Fontán Rodiño <gabilessto@gmail.com>
2026-01-08 13:04:58 +0100

    refactor: update build number to 6 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ val currentVersion: Version = Version.Beta(
     major = 1,
     minor = 0,
     patch = 0,
-    build = 5
+    build = 6
 )
 
 val versionCode by extra(currentVersion.toVersionCode())
