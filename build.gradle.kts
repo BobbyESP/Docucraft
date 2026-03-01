62b62df38b86880b6e7e294174674dc8ef753c98
Gabriel Fontán Rodiño <gabilessto@gmail.com>
2026-03-01 16:24:47 +0100

    chore: increment build number to 16 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ val currentVersion: Version = Version.Beta(
     major = 1,
     minor = 0,
     patch = 0,
-    build = 15
+    build = 16
 )
 
 val versionCode by extra(currentVersion.toVersionCode())
