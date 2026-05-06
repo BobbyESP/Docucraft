cb95dc32fd3dd2b88b2e34ac440dbe9edb8ba95c
Gabriel Fontán <gabilessto@gmail.com>
2026-05-06 17:30:13 +0200

    refactor: update library versions

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object ProjectConfig {
     const val minSdk = 24
-    const val compileSdk = 36
-    const val targetSdk = 36
+    const val compileSdk = 37
+    const val targetSdk = 37
 
     val javaVersion = JavaVersion.VERSION_17
     val jvmTarget = JvmTarget.JVM_17
